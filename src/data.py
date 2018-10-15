286fd04953d3ae2fdfab4de211502a35016d55c4
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-10-15 09:37:47 -0700

    Performance improvements in startup

--- src/data.py
+++ src/data.py
@@ -2,6 +2,7 @@
 
 import re
 import itertools
+import multiprocessing
 
 from tokenizer import Tokenizer, TokenizerState, \
     get_topk_keywords, get_relevant_k_keywords, \
@@ -19,7 +20,7 @@ EOS_token = 0
 
 Sentence = List[int]
 Bag = List[int]
-RawDataset = List[Tuple[List[str], str, str]]
+RawDataset = Iterable[Tuple[List[str], str, str]]
 ClassifySequenceDataset = List[Tuple[Sentence, int]]
 SequenceSequenceDataset = List[Tuple[Sentence, Sentence]]
 ClassifyBagDataset = List[Tuple[Bag, int]]
@@ -37,25 +38,45 @@ def extend(vector : List[int], length : int):
     assert len(vector) <= length
     return vector + [0] * (length - len(vector))
 
+def file_chunks(filepath : str, chunk_size : int):
+    with open(filepath, 'r') as f:
+        while True:
+            chunk = list(itertools.islice(f, chunk_size))
+            if len(chunk) == chunk_size:
+                while chunk[-1] != "-----\n":
+                    nextline = f.readline()
+                    if not nextline:
+                        break
+                    chunk += [nextline]
+                    assert len(chunk) < chunk_size * 2
+            elif len(chunk) == 0:
+                return
+            yield chunk
+
+def read_text_data_worker__(lines : List[str]) -> RawDataset:
+    def worker_generator():
+        with io.StringIO("".join(lines)) as f:
+            t = read_tuple(f)
+            while t:
+                yield t
+                t = read_tuple(f)
+    return list(worker_generator())
+
 def read_text_data(data_path : str,  max_size:Optional[int]=None) -> RawDataset:
     data_set = []
-    with open(data_path, mode="r") as data_file:
-        t = read_tuple(data_file)
-        counter = 0
-        while t and (not max_size or counter < max_size):
-            hyps, context, tactic = t
-            counter += 1
-            data_set.append((hyps, context, tactic))
-            t = read_tuple(data_file)
-    assert len(data_set) > 0
-    return data_set
+    with multiprocessing.Pool(None) as pool:
+        line_chunks = file_chunks(data_path, 32768)
+        data_chunks = pool.imap_unordered(read_text_data_worker__, line_chunks)
+        result = list(itertools.islice(itertools.chain.from_iterable(data_chunks),
+                                       max_size))
+        return result
 
 def filter_data(data : RawDataset, pair_filter : ContextFilter) -> RawDataset:
-    return [(hyps, goal, tactic)
+    return ((hyps, goal, tactic)
             for ((hyps, goal, tactic), (next_hyps, next_goal, next_tactic)) in
             zip(data, data[1:])
             if pair_filter({"goal": goal, "hyps" : hyps}, tactic,
-                           {"goal": next_goal, "hyps" : next_hyps})]
+                           {"goal": next_goal, "hyps" : next_hyps}))
 
 def encode_seq_seq_data(data : RawDataset,
                         context_tokenizer_type : Callable[[List[str], int], Tokenizer],
